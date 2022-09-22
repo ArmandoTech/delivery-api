21bedce786100d55a6b6bee00125f55acdf3d14e
Armando <104610494+ArmandoTech@users.noreply.github.com>
2022-09-22 16:35:53 -0500

    fix/users Schema fixed

--- src/models/users.js
+++ src/models/users.js
@@ -1,23 +1,27 @@
-import { model, Schema } from 'mongoose'
-import { client } from '../constants/roles'
+import { model, Schema } from "mongoose";
+import { client } from "../constants/roles";
 
-const UserSchema = new Schema({
-    deleted: {type: Boolean, default: false},
-    verify: {type: Boolean, default: false},
-    role: {type: String, default: client},
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    img: {type: String},
-    name: {type: String, required: true},
-    surname: {type: String, required: true},
-    phone: {type: Number},
-}, {
-    toJSON: (doc, data) => {
-        data.id = data._id,
-        delete data._id 
-        delete data.__v
-    }
-})
+const UserSchema = new Schema(
+	{
+		deleted: { type: Boolean, default: false },
+		verify: { type: Boolean, default: false },
+		role: { type: String, default: client },
+		username: { type: String, required: true },
+		email: { type: String, required: true },
+		password: { type: String, required: true },
+		img: { type: String },
+		name: { type: String, required: true },
+		surname: { type: String, required: true },
+		phone: { type: Number }
+	},
+	{
+		toJSON: (doc, data) => {
+			data.id = data._id;
+			delete data._id;
+			delete data.__v;
+		},
+		versionKey: false
+	}
+);
 
-export const User = model('User', UserSchema)
+export const User = model("User", UserSchema);
