c814e2ddef42dd457771310b8812f8588ebecaee
Armando <104610494+ArmandoTech@users.noreply.github.com>
2022-09-23 16:14:07 -0500

    feature/update users working

--- src/routes/users/users.router.js
+++ src/routes/users/users.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { deleteUserController } from "./deleteController/deleteUser.controller.js";
 import { getAllUsersController } from "./getController/getAllUsers.controller.js";
+import { updateUserController } from "./updateController/updateUser.controller.js";
 
 export const users = Router();
 
 users.route("/").get(getAllUsersController);
 users.route("/:id").delete(deleteUserController);
+users.route("/:id").patch(updateUserController);
---
+++ src/utils/updateUser.js
@@ -0,0 +1,16 @@
+import { User } from "../models/users.js";
+
+export const updateUser = async (query, body) => {
+	const { id } = query;
+	const { username, email, password, img, name, surname, phone } = body;
+	const user = User.findOneAndUpdate(id, {
+		username,
+		email,
+		password,
+		img,
+		name,
+		surname,
+		phone
+	});
+	return user;
+};
