2eada435132ed3b65cf7d5b3d6d0192feaccbf61
Fito Ikari <chris_fito@hotmail.com>
2022-09-24 17:29:38 -0500

    unnecessary controller deleted

--- src/routes/resetDB/resetController/reset.controller.js
+++
@@ -1,9 +0,0 @@
-import { resetDB } from "../../../utils/queries/resetBD.js";
-
-export const resetController = async (req, res, next) => {
-    try {
-        resetDB();
-    } catch (error) {
-        next(error);
-    }
-};
