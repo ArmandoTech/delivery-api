13fbd1ca66ec8102aafa8d4ef1ad41436cd6de30
edarcode <edarcode@gmail.com>
2022-09-24 15:22:10 -0500

    refactor() method find or create and route login

--- src/utils/findOrCreate.js
+++ src/utils/findOrCreate.js
@@ -1,9 +1,9 @@
 export const findOrCreate = async (Model, search, data) => {
-	const newSearch = [];
+	const formatSearch = [];
 	for (const [key, value] of Object.entries(search)) {
-		newSearch.push({ [key]: value });
+		formatSearch.push({ [key]: value });
 	}
-	const document = await Model.findOne({ $or: newSearch }).exec();
+	const document = await Model.findOne({ $or: formatSearch }).exec();
 	if (document) return [document, false];
 	else {
 		const newDocument = new Model({ ...search, ...data });
