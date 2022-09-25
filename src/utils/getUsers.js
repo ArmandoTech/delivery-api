2dcafa42581f045bb1d6cdded69c72f95e433d79
Armando <104610494+ArmandoTech@users.noreply.github.com>
2022-09-24 22:50:47 -0500

    feature/adding query params to filter on get all users

--- src/utils/getUsers.js
+++ src/utils/getUsers.js
@@ -2,12 +2,24 @@ import { LIMIT_USERS } from "../constants/limits.js";
 import { User } from "../models/users.js";
 
 export const getUser = async queries => {
-	const { limit = LIMIT_USERS, page = 0 } = queries;
-	const users = await User.find()
-		.skip(page * limit)
+	const { page = 0, limit = LIMIT_USERS } = queries;
+	const users = await User.find(addQueriesFind(queries))
 		.limit(limit)
+		.skip(page * limit)
 		.exec();
 
-	console.log(users);
-	return users;
+	const count = await User.countDocuments();
+	const totalPages = Math.ceil(count / limit);
+	return { users, count, totalPages, currenPage: page };
+};
+
+const addQueriesFind = queries => {
+	const { deleted, role, name, surname, email } = queries;
+	const queryFind = {};
+	if (deleted) queryFind.deleted = deleted;
+	if (role) queryFind.role = role;
+	if (name) queryFind.name = name;
+	if (surname) queryFind.surname = surname;
+	if (email) queryFind.email = email;
+	return queryFind;
 };
