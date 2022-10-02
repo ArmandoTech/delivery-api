e45cc69c532405555654e43e06798f9f7d48a76f
Fito Ikari <chris_fito@hotmail.com>
2022-10-02 15:07:56 -0500

    Added category filler for testing propouses

--- src/utils/fillDb/fillAllDb.js
+++ src/utils/fillDb/fillAllDb.js
@@ -1,5 +1,7 @@
+import { fillCategory } from "./fillCategory.js";
 import { fillUser } from "./fillUser.js";
 
 export const fillAllDb = async () => {
+	await fillCategory();
 	return await fillUser();
 };
---
+++ src/utils/fillDb/fillCategory.js
@@ -0,0 +1,9 @@
+import { categoriesMockup } from "../../data/categoriesMockup.js";
+import { createCategory } from "../categoryQueries/createCategory.js";
+import { generateArrayPromises } from "../generateArrayPromises.js";
+
+export const fillCategory = async () => {
+    const promiseCreateCategories = generateArrayPromises(categoriesMockup, createCategory);
+    const categoriesCreated = await Promise.all(promiseCreateCategories);
+    return categoriesCreated;
+};
