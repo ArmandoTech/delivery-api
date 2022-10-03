5a5a65001d1370ee59c903638e11056606d5a39f
edarcode <edarcode@gmail.com>
2022-10-02 21:27:36 -0500

    fix/ the method createUser now handles saving the user and throws err if needed

--- src/routes/auth/postController/register.controller.js
+++ src/routes/auth/postController/register.controller.js
@@ -1,17 +1,11 @@
-import {
-	CREDENTIALS_NOT_AVALIABLE,
-	VIRIFY_EMAIL
-} from "../../../constants/msgs.js";
+import { VIRIFY_EMAIL } from "../../../constants/msgs.js";
 import { createToken } from "../../../utils/createToken.js";
 import { createUser } from "../../../utils/createUser.js";
 import { sendVerificationEmail } from "../../../utils/sendVerificationEmail.js";
 
 export const registerController = async (req, res, next) => {
 	try {
-		const [user, created] = await createUser(req.body);
-		if (!created && user.verify)
-			return res.status(400).json({ msg: CREDENTIALS_NOT_AVALIABLE });
-		await user.save();
+		const user = await createUser(req.body);
 		const token = createToken({ userId: user._id }, "1h");
 		await sendVerificationEmail(user.email, token);
 		res.status(201).json({ msg: VIRIFY_EMAIL });
